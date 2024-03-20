9814c53a20d183daa8507ba87ec77534323a5372
Shubham Chopde <shubhamchopde090@gmail.com>
2024-03-20 12:25:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ShowData from './Screen/ShowData'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <ShowData/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/Screen/ShowData.jsx
@@ -0,0 +1,46 @@
+// import { URL } from 'url';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const ShowData = () => {
+  const [img, setimg] = useState("");
+  const [textValue, setTextValue] = useState("");
+
+  async function query(Data) {
+    const response = await axios.post(
+      "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
+      {
+        inputs: Data,
+      },
+      {
+        headers: {
+          Authorization: `Bearer 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`,
+        },
+        responseType: "blob",
+      }
+    );
+    console.log(response.data);
+    const imgurl = URL.createObjectURL(response.data);
+    setimg(imgurl);
+  }
+
+  return (
+    <>
+        <section className="w-[1280px] mx-auto "> 
+            <div className="text-center my-12 ">
+                <input
+                    type="text"
+                    className="border-2 py-2 px-4 mr-4"
+                    onChange={(e) => setTextValue(e.target.value)}
+                    />
+
+                <button className="bg-blue-400 py-2 px-6 rounded text-xl " onClick={() => query(textValue)}>Search results;</button>
+
+                {img && <img src={img} alt="Image from Blob" />}
+            </div>
+        </section>
+    </>
+  );
+};
+
+export default ShowData;
